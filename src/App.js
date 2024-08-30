6cfc491f89b58c90d7e5a061eb5565f6d6e9d00a
Ritika Nankar <112073087+Ritika128@users.noreply.github.com>
2024-08-31 02:43:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,17 +3,26 @@ import './App.css';
 import Header from './components/Header';
 import HeroSection from './components/HeroSection';
 import About from './components/About';
-import Services from './components/Services';
 import Footer from './components/Footer';
+import Features from './components/Features';
+import FAQs from './components/FAQs';
+import Testimonial from './components/Testimonial'
+import EscrowForm from './components/EscrowForm';
+import Spline from '@splinetool/react-spline';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <HeroSection />
-      <About />
-      <Services />
-      <Footer />
+      <Spline className="app-background" scene="https://prod.spline.design/fmsIAQTVvZBiU4s5/scene.splinecode" />
+      <div className="app-content">
+        <Header />
+        <HeroSection />
+        <About />
+        <Features/>
+        <Testimonial/>
+        <FAQs/>
+        <Footer />
+      </div>
     </div>
   );
 }
