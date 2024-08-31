ca6b94aef8e57c3cf3bda815259191bcaa333520
adiawaskar <142433196+adiawaskar@users.noreply.github.com>
2024-08-31 19:18:11 +0530

    Updated App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import Sidebar from './components/Sidebar/Sidebar';
+import Content from './components/Content/Content';
+import Profile from './components/Profile/Profile';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  // State to track the active item
+  const [activeItem, setActiveItem] = useState('Dashboard');
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='dashboard'>
+      <Sidebar activeItem={activeItem} setActiveItem={setActiveItem} />
+      <div className="dashboard--content">
+        <Content activeItem={activeItem} />
+        <Profile />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
+
+
