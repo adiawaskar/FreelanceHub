8e3e58cf8accc90deecc6633907fad45d4ad5703
Ritika Nankar <112073087+Ritika128@users.noreply.github.com>
2024-08-31 19:51:49 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,6 +10,9 @@ const Header = () => {
           <li><a href="#home">Home</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="#services">Services</a></li>
+          <li><a href="#login">Login</a></li>
+          <li><a href="#signup">Signup</a></li>
+          
         </ul>
       </nav>
     </header>
