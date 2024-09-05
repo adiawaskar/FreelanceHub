6a7c2d3dbedd782cc98cab356453c106279a0029
Ritika Nankar <112073087+Ritika128@users.noreply.github.com>
2024-09-05 08:05:12 +0530

    Create Content.jsx

---
+++ ClientProfile/components/Content/Content.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+// import ContentHeader from '../ContentHeader/ContentHeader'
+import "./Content.css";
+import Card from '../Card/Card';
+
+const Content = () => {
+  return (
+    <div className="content">
+        <Card />
+        
+    </div>
+  )
+}
+
+export default Content
